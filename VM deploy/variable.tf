bf705fb3009eb765a0a5f5ebfc47061b32abd2a2
ismorris <ismorris@Microsoft.com>
2020-05-13 16:55:49 -0700

    second commit

--- VM deploy/variable.tf
+++ VM deploy/variable.tf
@@ -6,24 +6,24 @@ variable "vm_size" {
   default = "Standard_DS1_v2"
 }
 variable "hostname" {
-  default = "myterrafvm"
+  default = "myterraformvm"
 }
 variable "image_uri" {
-  default = "/subscriptions/f2aeaccd-73bf-45ef-8603-946e33fa5570/resourceGroups/terraformRG/providers/Microsoft.Compute/galleries/ismorrisSIG/images/terrimage/versions/0.0.1"
+  default = ""
 }
 variable "os_type" {
   default = "windows"
 }
 variable "admin_username" {
-  default = "merrix"
+  default = "admin"
 }
 variable key_vault_name {
   description = "Name of the keyVault"
-  default     = "testkeyVault123"
+  default     = ""
 }
 variable encryption_key_url {
   description = "URL to encrypt Key"
-  default     = "https://freshvault.vault.azure.net/keys/encryptionkey/4702cbcb8bb044109e987696c9f8c5f9"
+  default     = ""
 }
 variable encryption_algorithm {
   description = " Algo for encryption"
